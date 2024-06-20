4e2fd849afc2cf6a9df064aeec21c20a2a3412d5
stutiwhy <161051299+stutiwhy@users.noreply.github.com>
2024-06-20 16:19:54 +0530

    MERN Stack started

---
+++ MERN/index.js
@@ -0,0 +1,68 @@
+console.log("hello, stuti!")
+console.log(357879)
+console.log("hello my name is " + "stuti" )
+console.log(typeof 234)
+console.log(typeof "stuti")
+
+var a = "Hello"
+console.log(a)
+
+var s = "hello, " + "my name is " + "stuti"
+console.log(s)
+
+var b = Number("34") * 4
+console.log(b)
+
+var n = null
+console.log(n)
+
+var b = "435" * 8
+console.log(b)
+
+var p = pi
+console.log(p)
+
+console.log(a)
+var a = 100
+var a = 23
+
+let l = [1, 2, 3, 4, 5]
+l[2] = "44"
+// l = ""
+console.log(l)
+
+let d = {
+    name : "stuti",
+    age : 17
+}
+
+console.log(d, d.name, d.age)
+
+let person = {
+    name : "stuti",
+    age : 17,
+    hobby : ["reading", 'gaming', 'gym']
+}
+
+console.log(person)
+
+person.hobby[2] = "music"
+
+console.log(person)
+
+let s = 'vivek'
+console.log(s.toUpperCase())
+
+let b = 1
+console.log(b === '1')
+
+console.log('stuti' === 'Stuti')
+
+var 2_$myVarVar = 1
+
+let x = 5 + "5"
+console.log(10 == '10')
+
+let a = 10
+a++
+console.log(a)
