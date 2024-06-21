1ed580e73592d697d2abe0a947e7f1d29d85b2a1
stutiwhy <161051299+stutiwhy@users.noreply.github.com>
2024-06-21 15:45:33 +0530

    uncommented

--- sixth.py
+++ sixth.py
@@ -1,56 +1,56 @@
-# exception handling
-# try - except syntax: 
-# try:
-#     pass
-# except Exception as e:
-#     print(e)
-# try:
-#     # print(9 / 0)
-#     # print(y)
+exception handling
+try - except syntax: 
+try:
+    pass
+except Exception as e:
+    print(e)
+try:
+    # print(9 / 0)
+    # print(y)
     
-#     a = input("Enter a : ")
-#     # a = int(a)
-#     b = a * a
-#     print(b)
-# except Exception as e:
-#     print(e)
-# finally:
-#     print("Good bye !")
+    a = input("Enter a : ")
+    # a = int(a)
+    b = a * a
+    print(b)
+except Exception as e:
+    print(e)
+finally:
+    print("Good bye !")
 
-# types of errors
-# IOError
+types of errors
+IOError
 
-# try:
-#     # with open("non_existent_file.txt", "r"):
-#     #     content = file.read() # IOError
+try:
+    # with open("non_existent_file.txt", "r"):
+    #     content = file.read() # IOError
 
-#     n = int("hehe") # ValueError
+    n = int("hehe") # ValueError
 
-#     # res = '2' + 2 # TypeError
+    # res = '2' + 2 # TypeError
     
-#     # Logging.log(round(24.234 / 3.92, 5)) # module(????)
+    # Logging.log(round(24.234 / 3.92, 5)) # module(????)
 
-# except IOError as i:
-#     print("IOError")
-# except ValueError as v:
-#     print("ValueError")
-# except ArithmeticError as a:
-#     print("ArithmeticError")
-# except TypeError as t:
-#     print("TypeError")
-# except FloatingPointError as f:
-#     print("FloatingPointError")
-# finally:
-#     print("Good bye !")
+except IOError as i:
+    print("IOError")
+except ValueError as v:
+    print("ValueError")
+except ArithmeticError as a:
+    print("ArithmeticError")
+except TypeError as t:
+    print("TypeError")
+except FloatingPointError as f:
+    print("FloatingPointError")
+finally:
+    print("Good bye !")
 
-# installed pip and pandas
+installed pip and pandas
 
-# from third import greet
-# greet("Stuti", "You are amazing !")
+from third import greet
+greet("Stuti", "You are amazing !")
 
-# from package import mod1 as m
-# x = m.mul(5,9)
-# print(x)
+from package import mod1 as m
+x = m.mul(5,9)
+print(x)
 
 from package.pack1 import *
 print(mod2.add(3,8))
